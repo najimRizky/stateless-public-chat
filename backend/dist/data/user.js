aa5c93e0ae993e5075ad670eb844f8284734a610
najimRizky <najim.rizky@student.umn.ac.id>
2022-12-24 19:48:43 +0700

    refactor: re add dist to gitignore

--- backend/dist/data/user.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roomUserIsTyping = exports.roomActiveUsers = exports.usersIsTyping = exports.activeUsers = void 0;
-exports.activeUsers = {};
-exports.usersIsTyping = [];
-exports.roomActiveUsers = {};
-exports.roomUserIsTyping = {};
